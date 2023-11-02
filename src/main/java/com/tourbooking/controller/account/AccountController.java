10ed9ad4e0257e57d24162ebee6547a601e0bae5
LyDoan0302 <138420217+LyDoan0302@users.noreply.github.com>
2023-11-02 16:24:23 +0700

    test

--- src/main/java/com/tourbooking/controller/account/AccountController.java
+++ src/main/java/com/tourbooking/controller/account/AccountController.java
@@ -1,4 +1,13 @@
 package com.tourbooking.controller.account;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.persistence.GeneratedValue;
+
+@Controller
+@RequestMapping("/account")
 public class AccountController {
+//    comment
 }
